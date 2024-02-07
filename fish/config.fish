bab27d159756e7c82f35837de2105945de83f0d7
Lengyue57 <1210625283@qq.com>
2024-02-07 15:51:35 +0800

    Add graphic checker

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,14 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+  # Commands to run in interactive sessions can go here
 
+  alias fv=ranger
+
+  if test -n "$XDG_CURRENT_DESKTOP"
+    # If graphic
     alias e=neovide
-    alias fv=ranger
+  else
+    # If not graphic
+    set -g -x EDITOR nvim
+    alias e=nvim
+  end
 end
