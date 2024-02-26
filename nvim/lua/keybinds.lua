1c44e47691644cd8d83cc18e79ddb23c579b3078
Lengyue57 <1210625283@qq.com>
2024-02-26 10:45:31 +0800

    Change keybinds

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -1,61 +1,60 @@
 local keymap = vim.api.nvim_set_keymap
 local opt = { noremap = true, silent = true }
 
-vim.g.mapleader='<space>'
-vim.g.maplocalleader='<space>'
+vim.g.mapleader="<space>"
+vim.g.maplocalleader="<space>"
 
-keymap('t', '<ESC>', '<C-\\><C-n>', opt)
+keymap("t", "<ESC>", "<C-\\><C-n>", opt)
 
-keymap('n', '<C-e>', '<C-e><C-e><C-e>', opt)
-keymap('n', '<C-y>', '<C-y><C-y><C-y>', opt)
+keymap("n", "<C-e>", "<C-e><C-e><C-e>", opt)
+keymap("n", "<C-y>", "<C-y><C-y><C-y>", opt)
 
-keymap('n', 'A', 'g_a', opt)
-keymap('n', 'n', 'gn', opt)
-keymap('n', 'N', 'gN', opt)
-keymap('n', 'j', 'gj', opt)
-keymap('n', 'k', 'gk', opt)
-keymap('n', '<Up>', 'g<Up>', opt)
-keymap('n', '<Down>', 'g<Down>', opt)
-keymap('v', 'j', 'gj', opt)
-keymap('v', 'k', 'gk', opt)
-keymap('v', '<Up>', 'g<Up>', opt)
-keymap('v', '<Down>', 'g<Down>', opt)
-keymap('v', 'n', '<ESC>ngn', opt)
-keymap('v', 'N', '<ESC>NgN', opt)
+keymap("",  "j", "gj", opt)
+keymap("",  "k", "gk", opt)
+keymap("i", "<Up>",   "<C-o>g<Up>", opt)
+keymap("i", "<Down>", "<C-o>g<Down>", opt)
 
-keymap('n', '<A-q>', '<C-w>q', opt)
-keymap('n', '<A-w>', '<C-w>w', opt)
-keymap('n', '<A-r>', '<C-w>r', opt)
-keymap('n', '<A-c>', '<C-w>c', opt)
+keymap("n",  "n", "gn", opt)
+keymap("n",  "N", "gN", opt)
+keymap("v",  "n", "<ESC>ngn", opt)
+keymap("v",  "N", "<ESC>NgN", opt)
+keymap("o",  "n", "gn", opt)
+keymap("o",  "N", "gN", opt)
 
-keymap('n', '<A-h>', '<C-w>h', opt)
-keymap('n', '<A-j>', '<C-w>j', opt)
-keymap('n', '<A-k>', '<C-w>k', opt)
-keymap('n', '<A-l>', '<C-w>l', opt)
+-- 窗格分割
+keymap("n", "<A-\">", "<C-w>s", opt)
+keymap("n", "<A-|>",  "<C-w>v", opt)
 
-keymap('n', '<A-H>', '<C-w>H', opt)
-keymap('n', '<A-J>', '<C-w>J', opt)
-keymap('n', '<A-K>', '<C-w>K', opt)
-keymap('n', '<A-L>', '<C-w>L', opt)
+-- 关闭窗格
+keymap("n", "<A-q>", "<C-w>q", opt)
+keymap("n", "<A-c>", "<C-w>c", opt)
 
-keymap('n', '<A-Left>', '<C-w><Left>', opt)
-keymap('n', '<A-Down>', '<C-w><Down>', opt)
-keymap('n', '<A-Up>', '<C-w><Up>', opt)
-keymap('n', '<A-Right>', '<C-w><Right>', opt)
+-- 窗格跳转
+keymap("n", "<A-w>", "<C-w>w", opt)
+keymap("n", "<A-h>", "<C-w>h", opt)
+keymap("n", "<A-j>", "<C-w>j", opt)
+keymap("n", "<A-k>", "<C-w>k", opt)
+keymap("n", "<A-l>", "<C-w>l", opt)
+keymap("n",  "<A-Up>",    "<C-w><Up>", opt)
+keymap("n",  "<A-Down>",  "<C-w><Down>", opt)
+keymap("n",  "<A-Left>",  "<C-w><Left>", opt)
+keymap("n",  "<A-Right>", "<C-w><Right>", opt)
 
-keymap('n', '<A-">', '<C-w>s', opt)
-keymap('n', '<A-|>', '<C-w>v', opt)
+-- 窗格移动
+keymap("n", "<A-r>", "<C-w>r", opt)
+keymap("n", "<A-H>", "<C-w>H", opt)
+keymap("n", "<A-J>", "<C-w>J", opt)
+keymap("n", "<A-K>", "<C-w>K", opt)
+keymap("n", "<A-L>", "<C-w>L", opt)
 
-keymap('n', '<A-<>', '<C-w><', opt)
-keymap('n', '<A->>', '<C-w>>', opt)
-keymap('n', '<A-=>', '<C-w>=', opt)
-keymap('n', '<A-+>', '<C-w>+', opt)
-keymap('n', '<A-->', '<C-w>-', opt)
+-- 调整窗格大小
+keymap("n", "<A-<>", "<C-w><", opt)
+keymap("n", "<A->>", "<C-w>>", opt)
+keymap("n", "<A-=>", "<C-w>=", opt)
+keymap("n", "<A-+>", "<C-w>+", opt)
+keymap("n", "<A-->", "<C-w>-", opt)
 
-keymap('v', '<', '<gv', opt)
-keymap('v', '>', '>gv', opt)
+keymap("v", "<", "<gv", opt)
+keymap("v", ">", ">gv", opt)
 
-keymap('n', '<C-\\>', '<cmd>Ex<CR>', opt)
-keymap('i', '<C-\\>', '<cmd>Ex<CR>', opt)
-keymap('v', '<C-\\>', '<cmd>Ex<CR>', opt)
-
+keymap("n", "<C-\\>", "<CMD>Ex<CR>", opt)
