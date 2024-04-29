d127603ea3ec651468137e1fee33d32c85ac7e14
Lengyue57 <61723363+Lengyue57@users.noreply.github.com>
2024-04-29 14:10:26 +0800

    Optimize the initializer

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,21 @@
 if status is-interactive
   # Commands to run in interactive sessions can go here
 
-  if test -n "$XDG_CURRENT_DESKTOP" || test -n "$WSL_DISTRO_NAME"
-    # If graphic
-
-    # envs
-    set -g -x IS_GRAPHIC TRUE
-  else
-    # If not graphic
+  if test -n "$CONFIG_INITED"
+    # init
 
     # envs
     set -g -x CONFIG_DIR ~/.config
     set -g -x MY_SCRIPT ../MyScript
+
+    set -g -x CONFIG_INITED TRUE
+  end
+
+  if test -n "$XDG_CURRENT_DESKTOP" || test -n "$WSL_DISTRO_NAME"
+    # If graphic
+
+    # envs
+    set -x IS_GRAPHIC TRUE
   end
 
   alias e=editor
