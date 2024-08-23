3d21c4150472377a135f48b7d6f010465ea9b3d3
Lengyue57 <61723363+Lengyue57@users.noreply.github.com>
2024-08-23 22:05:12 +0800

    Upgrade lazy.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
-require("plugins")
+require("plugin")
 require("lsp")
 require("basic")
 require("keybinds")
--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -1,17 +1,4 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup {
+return {
   {
     'nvim-lualine/lualine.nvim',
     dependencies = 'nvim-tree/nvim-web-devicons',
@@ -20,27 +7,21 @@ require("lazy").setup {
     end
   },
   -- 代码折叠
-  {
-    'kevinhwang91/nvim-ufo',
-    dependencies = 'kevinhwang91/promise-async',
-    config = function()
-      require("plugin-config/nvim-ufo")
-    end
-  },
+  -- {
+  --   'kevinhwang91/nvim-ufo',
+  --   dependencies = 'kevinhwang91/promise-async',
+  --   config = function()
+  --     require("plugin-config/nvim-ufo")
+  --   end
+  -- },
   -- 语法高亮
   {
     "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
     config = function()
       require("nvim-treesitter.install").prefer_git = true
       require("plugin-config/nvim-treesitter")
     end,
   },
-
-  -- LSP
-  {"williamboman/mason.nvim"},
-  {"williamboman/mason-lspconfig.nvim"},
-  {"neovim/nvim-lspconfig"},
 
   -- 自动补全
   {"hrsh7th/cmp-nvim-lsp"},
