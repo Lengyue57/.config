6bb2fda9a2a3b78d6a4bb23cb65f62395070d74d
Lengyue57 <1210625283@qq.com>
2024-08-24 00:59:56 +0800

    Change wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,13 @@
 local wez = require("wezterm")
 
 local conf = {
+  -- 字体
   font = wez.font_with_fallback({"UbuntuMono Nerd Font", "Source Han Sans SC"}),
   font_size = 10,
 
   color_scheme = 'GitHub Dark',
+  -- foreground = "#e6edf3",
+  -- background = "#0d1117",
 
   enable_tab_bar = true,
   hide_tab_bar_if_only_one_tab = true,
@@ -12,6 +15,7 @@ local conf = {
   tab_bar_at_bottom = true,
   enable_scroll_bar = false,
 
+  -- 窗口边距
   window_padding = {
     top = 8,
     bottom = 0,
@@ -19,8 +23,30 @@ local conf = {
     right = 2,
   },
 
+  -- 窗口透明度
   window_background_opacity = 0.8,
   text_background_opacity = 0.8,
+
+  enable_wayland = false,
+
+  -- 按键绑定
+  keys = {
+    {
+      mods = "CMD",
+      key = "C",
+      action = wez.action.CopyTo "ClipboardAndPrimarySelection",
+    },
+    {
+      mods = "CMD",
+      key = "V",
+      action = wez.action.PasteFrom "Clipboard",
+    },
+    {
+      mods = "CMD",
+      key = "V",
+      action = wez.action.PasteFrom "PrimarySelection",
+    },
+  },
 }
 
 return conf
