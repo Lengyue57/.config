81010cf817ef150a1c19dd7fb957554536bf9060
Lengyue57 <61723363+Lengyue57@users.noreply.github.com>
2024-09-13 15:29:22 +0800

    Add flash.nvim plugin

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -1,4 +1,26 @@
 return {
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {
+      jump = {
+        nohlsearch = true,
+        autojump = true,
+      },
+      label = {
+        distance = true,
+      },
+      modes = {
+        search = {
+          enabled = true,
+        },
+      },
+    },
+    keys = {
+      { "r", mode = "o", function() require("flash").remote() end },
+    }
+  },
+
   -- fcitx 自动切换输入法
   (vim.fn.has("win64") == 0) and {"alohaia/fcitx.nvim",
     opts = {
