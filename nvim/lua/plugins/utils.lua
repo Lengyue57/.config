09bfa9c4f94e2367b171f872e35000d31acb4234
Lengyue57 <61723363+Lengyue57@users.noreply.github.com>
2024-09-13 15:28:03 +0800

    Rebuild nvim folding config

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -1,14 +1,4 @@
 return {
-  -- 代码折叠
-  -- {
-  --   'kevinhwang91/nvim-ufo',
-  --   dependencies = 'kevinhwang91/promise-async',
-  --   config = function()
-  --     require("plugin-config/nvim-ufo")
-  --   end
-  -- },
-
-
   -- fcitx 自动切换输入法
   (vim.fn.has("win64") == 0) and {"alohaia/fcitx.nvim",
     opts = {
