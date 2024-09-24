e2972c2478c27d6cb77a51ebbe35acb58cc25f87
Lengyue57 <61723363+Lengyue57@users.noreply.github.com>
2024-09-24 16:23:39 +0800

    Update nvim colorscheme

--- nvim/colors/github_dark.lua
+++ nvim/colors/github_dark.lua
@@ -1,82 +1,161 @@
-local function setup_highlights(highlight_groups)
-  for group_name, group_props in pairs(highlight_groups) do
-    if group_props == nil then
-      goto continue
-    end
-
-    if #group_props == 0 then
-      vim.api.nvim_set_hl(0, group_name, { link = "Normal" })
-    end
-
-    vim.api.nvim_set_hl(0, group_name, group_props)
-  end
-
-  ::continue::
-end
-
 local colors = {
-  white   = "#e6edf3",
-  bg      = "#0d1117",
-  ui_bg   = "#010409",
-  silver  = "#8b949e",
-  gray    = "#171b22",
-  red     = "#ff7b72",
-  lime    = "#7ee787",
-  blue    = "#79c0ff",
-  purple  = "#d2a8ff",
-  skyblue = "#a5d6ff",
-  yellow  = "#e3b341",
-  orenge  = "#ffa657",
+  white     = "#e6edf3",
+  silver    = "#8b949e",
+  lightgray = "#262c36",
+  gray      = "#171b22",
+  darkgray  = "#0d1117",
+  black     = "#010409",
+  red       = "#ff7b72",
+  lime      = "#7ee787",
+  blue      = "#79c0ff",
+  purple    = "#d2a8ff",
+  skyblue   = "#a5d6ff",
+  yellow    = "#e3b341",
+  orange    = "#ffa657",
 }
 
-setup_highlights {
-  NonText       = { fg = colors.gray },
-  Normal        = { fg = colors.white, bg = colors.bg },
-  LineNr        = { fg = colors.silver },
-  CursorLine    = { bg = colors.gray },
-  CursorLineNr  = { fg = colors.white },
+local highlight_groups = {
+  NonText                 = { fg = colors.gray },
+  Normal                  = { fg = colors.white, bg = colors.darkgray },
+  LineNr                  = { fg = colors.silver },
+  CursorLine              = { bg = colors.gray },
+  CursorLineNr            = { fg = colors.white },
+  Visual                  = { bg = colors.lightgray },
 
-  FloatBorder   = { fg = colors.silver },
+  FloatBorder             = { fg = colors.silver },
 
-  SignColumn    = {},
-  ColorColumn   = { bg = colors.gray },
+  SignColumn              = {},
+  ColorColumn             = { bg = colors.gray },
 
-  MsgArea       = { bg = colors.ui_bg },
-  ErrorMsg      = { fg = colors.red },
-  WarningMsg    = { fg = colors.yellow },
+  Pmenu                   = { bg = colors.gray },
 
+  -- 消息
+  MsgArea                 = { bg = colors.black },
+  ErrorMsg                = { fg = colors.red },
+  WarningMsg              = { fg = colors.yellow },
+  DiagnosticError         = "ErrorMsg",
+  DiagnosticWarn          = "WarningMsg",
+  DiagnosticInfo          = { fg = colors.white },
+  DiagnosticHint          = { fg = colors.skyblue },
+  DiagnosticOK            = { fg = colors.lime },
 
-  TabLine       = { fg = colors.silver, bg = colors.gray },
-  TabLineFill   = { bg = colors.ui_bg },
+  --
+  Special                 = { bold = true },
+  Url                     = { fg = colors.blue, underline = true },
+  ["@text.uri"          ] = "Url",
+  ["@text.reference"    ] = { fg = colors.blue, underline = true, },
+  -- ["@text.literal"      ] = { bg = colors.gray },
 
-  Folded        = { fg = colors.silver, bg = colors.gray },
+  TabLine                 = { fg = colors.silver, bg = colors.gray },
+  TabLineFill             = { bg = colors.black },
 
-  Statement     = { fg = colors.red },
-  Identifier    = { italic = true },
-  Function      = { fg = colors.purple, nocombine = true },
-  ["@variable"] = { link = "Identifier" },
-  Type          = { fg = colors.orenge },
-  Constant      = { fg = colors.blue },
-  String        = { fg = colors.skyblue },
-  Character     = { link = "String" },
-  Comment       = { fg = colors.silver },
-  Special       = { bold = true },
-  PreProc       = { fg = colors.purple },
+  Folded                  = { fg = colors.silver, bg = colors.gray },
 
-  Pmenu         = { bg = colors.ui_bg },
+  -- Treesitter
+  ["@comment"           ] = { fg = colors.silver },
+  ["@keyword"           ] = { fg = colors.red },
+  ["@repeat"            ] = "@keyword",
+  ["@conditional"       ] = "@keyword",
+  ["@type.qualifier"    ] = "@keyword",
+  ["@include"           ] = "@keyword",
+  ["@builtin"           ] = "@keyword",
+  ["@parameter"         ] = { bold = true },
+  ["@function"          ] = { fg = colors.purple },
+  ["@Function.builtin"  ] = {},
+  ["@method"            ] = "@function",
+  ["@variable"          ] = { italic = true },
+  ["@variable.builtin"  ] = {},
+  ["@variable.parameter"] = {},
+  ["@property"          ] = { "@variable", fg = colors.blue },
+  ["@type"              ] = { fg = colors.orange },
+  ["@type.builtin"      ] = {},
+  Identifier              = "Normal",
+  Comment                 = "@comment",
+  Repeat                  = "@repeat",
+  Conditional             = "@conditional",
+  Function                = "@function",
+  Parameter               = "@parameter",
+  Type                    = "@type",
+  Constant                = { fg = colors.blue },
+  String                  = { fg = colors.skyblue },
+  Character               = "String",
+  PreProc                 = { fg = colors.purple },
+  ["@delimiter"]          = { fg = colors.white },
+  ["@tag"               ] = { fg = colors.lime },
+  ["@tag.attribute"     ] = { fg = colors.blue },
+  ["@tag.delimiter"     ] = { "@delimiter" },
+  Tag                     = "@tag",
+  Delimiter               = "@delimiter",
 
   -- mark language specific
   -- xml
-  xmlTagName    = { fg = colors.lime },
-  xmlTag        = {},
-  xmlEndTag     = { link = "xmlTag" },
-  xmlAttrib     = { fg = colors.blue },
-  xmlEqual      = {},
+  xmlTagName              = "@tag",
+  xmlTag                  = { link = "@tag.delimiter" },
+  xmlEndTag               = "xmlTag",
+  xmlAttrib               = { link = "@tag.attribute" },
+  xmlEqual                = {},
   -- html
-  htmlTagN      = { link = "htmlTagName" },
-  htmlTagName   = { link = "xmlTagName" },
-  htmlTag       = { link = "xmlTag" },
-  htmlEndTag    = { link = "xmlEndTag" },
-  htmlArg       = { link = "xmlAttrib" },
-  htmlEqual     = { link = "xmlEqual" },
+  htmlTagName             = "xmlTagName",
+  htmlTagN                = "htmlTagName",
+  htmlTag                 = "xmlTag",
+  htmlEndTag              = "xmlEndTag",
+  htmlArg                 = "xmlAttrib",
+  htmlEqual               = "xmlEqual",
 }
+
+local function parse_group(group_name)
+  local group_opts = highlight_groups[group_name]
+  local dep_groups = {}
+  local result = {}
+
+  if string.match(group_name, "^@.+$") then
+    result.nocombine = true
+
+    if group_opts == nil then
+      group_opts = {}
+    end
+  end
+
+  if type(group_opts) == "string" then
+    result = parse_group(group_opts)
+  else
+    -- 整理依赖项
+    if string.match(group_name, "^@.+%.[^.]+$") then
+      table.insert(dep_groups, "@" .. string.match(group_name, "^@(.+)%.[^.]+$"))
+      table.insert(dep_groups, "@" .. string.match(group_name, "^@.+%.([^.]+)$"))
+    end
+
+    for _, dep_group_name in ipairs(group_opts) do
+      if type(dep_group_name) == "string" then
+        table.insert(dep_groups, dep_group_name)
+      end
+    end
+
+    -- 处理依赖项
+    for _, dep_group_name in ipairs(dep_groups) do
+      for dep_opt_name, dep_opt_val in pairs(parse_group(dep_group_name)) do
+        result[dep_opt_name] = dep_opt_val
+      end
+    end
+
+    -- 处理自身选项
+    for opt_name, opt_val in pairs(group_opts) do
+      if type(opt_name) ~= "number" and opt_name ~= "link" then
+        result[opt_name] = opt_val
+      end
+    end
+  end
+
+  return result
+end
+
+for group_name, _ in pairs(highlight_groups) do
+  local set_hl_opts = parse_group(group_name)
+
+  if next(set_hl_opts) == nil then
+    vim.api.nvim_set_hl(0, group_name, { link = "Normal" })
+    vim.api.nvim_set_hl(0, group_name, {})
+  else
+    vim.api.nvim_set_hl(0, group_name, set_hl_opts)
+  end
+end
