1882590afeede30e42d62452f8d94fed14dcbaaa
Lengyue57 <61723363+Lengyue57@users.noreply.github.com>
2024-10-25 14:00:54 +0800

    Update lualine.nvim config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,10 +1,65 @@
+local colors = {
+  white     = "#e6edf3",
+  silver    = "#8b949e",
+  lightgray = "#262c36",
+  gray      = "#171b22",
+  darkgray  = "#0d1117",
+  black     = "#010409",
+  red       = "#ff7b72",
+  lime      = "#7ee787",
+  blue      = "#79c0ff",
+  purple    = "#d2a8ff",
+  skyblue   = "#a5d6ff",
+  yellow    = "#e3b341",
+  orange    = "#ffa657",
+}
+
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = "nvim-tree/nvim-web-devicons",
   opts = {
     options = {
       icons_enabled = true,
-      theme = "auto",
+      theme = {
+        normal = {
+          a = {
+            bg = colors.silver,
+            fg = colors.black,
+            gui = "bold",
+          },
+          b = {
+            bg = colors.black,
+            fg = colors.white,
+          },
+        },
+        insert = {
+          a = {
+            bg = colors.yellow,
+            fg = colors.black,
+            gui = "bold",
+          },
+        },
+        visual = {
+          a = {
+            bg = colors.lime,
+            fg = colors.black,
+            gui = "bold",
+          },
+        },
+        replace = {
+          a = {
+            bg = colors.red,
+            fg = colors.black,
+            gui = "bold",
+          },
+        },
+        inactive = {
+          a = {
+            bg = colors.gray,
+            fg = colors.white,
+          },
+        },
+      },
       component_separators = { left = "", right = ""},
       section_separators = { left = "", right = ""},
       disabled_filetypes = {
